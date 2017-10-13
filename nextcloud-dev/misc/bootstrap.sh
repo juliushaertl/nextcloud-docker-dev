d43e8827280381cc72a8b1d636daefe4d756e3da
Julius Härtl <jus@bitgrid.net>
2017-10-13 12:48:17 +0200

    Enable xdebug properly and only run setup when NC is not installed

--- nextcloud-dev/misc/bootstrap.sh
+++ nextcloud-dev/misc/bootstrap.sh
@@ -1,57 +1,72 @@
 #!/bin/bash
 
-cp /root/config.php $WEBROOT/config/config.php
-
 OCC="sudo -u www-data $WEBROOT/occ"
 
-if [ "$SQL" = "mysql" ]
+STATUS=`$OCC status`
+
+setup() {
+    cp /root/config.php $WEBROOT/config/config.php
+
+    if [ "$SQL" = "mysql" ]
+    then
+        # wait until mysql is ready
+        while ! timeout 1 bash -c "echo > /dev/tcp/nc-database-mysql/3306"; do sleep 2; done
+        cp /root/autoconfig_mysql.php $WEBROOT/config/autoconfig.php
+        SQLHOST=nc-database-mysql
+    fi
+
+    if [ "$SQL" = "pgsql" ]
+    then
+        while ! timeout 1 bash -c "echo > /dev/tcp/nc-database-postgres/5432"; do sleep 2; done
+        cp /root/autoconfig_pgsql.php $WEBROOT/config/autoconfig.php
+        SQLHOST=nc-database-postgres
+    fi
+
+    if [ "$SQL" = "oci" ]
+    then
+        cp /root/autoconfig_oci.php $WEBROOT/config/autoconfig.php
+    fi
+
+    cp /root/config.php /var/www/html/config/config.php
+
+    chown -R www-data:www-data $WEBROOT/data $WEBROOT/config $WEBROOT/apps-extra
+
+    USER=admin
+    PASSWORD=admin
+    if [ "$NEXTCLOUD_AUTOINSTALL" = "YES" ]
+    then
+        echo "Starting auto installation"
+        if [ "$SQL" = "oci" ]; then
+            $OCC maintenance:install --admin-user=$USER --admin-pass=$PASSWORD --database=$SQL --database-name=xe --database-host=$SQLHOST --database-user=system --database-pass=oracle
+        else
+            $OCC maintenance:install --admin-user=$USER --admin-pass=$PASSWORD --database=$SQL --database-name=nextcloud --database-host=$SQLHOST --database-user=nextcloud --database-pass=nextcloud
+        fi;
+
+        for app in $NEXTCLOUD_AUTOINSTALL_APPS; do
+            echo "Enable app ${app}"
+            $OCC app:enable $app
+        done
+    fi;
+
+
+    echo "Starting server using $SQL database…"
+
+    tail --follow --retry $WEBROOT/data/nextcloud.log &
+
+    echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini 
+    echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini 
+    echo "xdebug.remote_connect_back=on" >> /usr/local/etc/php/conf.d/xdebug.ini 
+    echo "xdebug.idekey=PHPSTORM" >> /usr/local/etc/php/conf.d/xdebug.ini
+    echo "xdebug.remote_host=172.17.0.1" >> /usr/local/etc/php/conf.d/xdebug.ini
+
+    # run custom shell script from nc root
+
+    bash /var/www/html/nc-dev-autosetup.sh
+}
+
+if [[ "$STATUS" != *"installed: true"* ]]
 then
-	# wait until mysql is ready
-	while ! timeout 1 bash -c "echo > /dev/tcp/nc-database-mysql/3306"; do sleep 2; done
-	cp /root/autoconfig_mysql.php $WEBROOT/config/autoconfig.php
-    SQLHOST=nc-database-mysql
+    setup
 fi
-
-if [ "$SQL" = "pgsql" ]
-then
-	while ! timeout 1 bash -c "echo > /dev/tcp/nc-database-postgres/5432"; do sleep 2; done
-	cp /root/autoconfig_pgsql.php $WEBROOT/config/autoconfig.php
-    SQLHOST=nc-database-postgres
-fi
-
-if [ "$SQL" = "oci" ]
-then
-	cp /root/autoconfig_oci.php $WEBROOT/config/autoconfig.php
-fi
-
-chown -R www-data:www-data $WEBROOT/data $WEBROOT/config $WEBROOT/apps-extra
-
-USER=admin
-PASSWORD=admin
-if [ "$NEXTCLOUD_AUTOINSTALL" = "YES" ]
-then
-	echo "Starting auto installation"
-	if [ "$SQL" = "oci" ]; then
-		$OCC maintenance:install --admin-user=$USER --admin-pass=$PASSWORD --database=$SQL --database-name=xe --database-host=$SQLHOST --database-user=system --database-pass=oracle
-	else
-		$OCC maintenance:install --admin-user=$USER --admin-pass=$PASSWORD --database=$SQL --database-name=nextcloud --database-host=$SQLHOST --database-user=nextcloud --database-pass=nextcloud
-	fi;
-
-	for app in $NEXTCLOUD_AUTOINSTALL_APPS; do
-		echo "Enable app ${app}"
-		$OCC app:enable $app
-	done
-fi;
-
-
-echo "Starting server using $SQL database…"
-
-tail --follow --retry $WEBROOT/data/nextcloud.log &
-
-echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini 
-echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini 
-echo "xdebug.remote_connect_back=on" >> /usr/local/etc/php/conf.d/xdebug.ini 
-echo "xdebug.idekey=PHPSTORM" >> /usr/local/etc/php/conf.d/xdebug.ini
-echo "xdebug.remote_host=172.17.0.1" >> /usr/local/etc/php/conf.d/xdebug.ini
 
 apache2-foreground
