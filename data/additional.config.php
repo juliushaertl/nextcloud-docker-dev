407f1ef85ab6ad2260cf44beafb7093079a39eb7
Julius Härtl <jus@bitgrid.net>
2019-05-05 12:30:24 +0200

    Add second Nextcloud instance for federation
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- data/additional.config.php
+++ data/additional.config.php
@@ -5,14 +5,17 @@ $CONFIG = array (
 		0 => 'localhost',
 		1 => 'local.dev.bitgrid.net',
 		2 => 'nextcloud.local.dev.bitgrid.net',
-
+		3 => 'nextcloud-federated.local.dev.bitgrid.net',
 		// docker internal ip / hostname required for blackfire support
-		3 => '192.168.21.8',
-		4 => 'nextcloud'
+		4 => '192.168.21.8',
+		5 => 'nextcloud'
 	],
 
 	'overwrite.cli.url' => 'https://nextcloud.local.dev.bitgrid.net',
 
 
 	'htaccess.RewriteBase' => '/',
+	
+	'overwriteprotocol' => 'https',
+
   );
