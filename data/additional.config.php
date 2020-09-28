b4deac44c294abd032469da0ec40d82ef5574a78
Julius Härtl <jus@bitgrid.net>
2020-09-28 09:44:55 +0200

    Add object store config examples
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- data/additional.config.php
+++ data/additional.config.php
@@ -1,7 +1,46 @@
 <?php
-$CONFIG = array (
-//  'htaccess.RewriteBase' => '/',
 
-  // Disable lookup server as it is not needed for our local ldap test users
-  'lookup_server' => ''
+// FIXME: hostname changes when container might be recreated, ideally this should be the instance id
+$hostname = gethostname();
+
+$CONFIG = array (
+	//  'htaccess.RewriteBase' => '/',
+
+	// Disable lookup server as it is not needed for our local ldap test users
+	'lookup_server' => '',
+
+	// Uncomment to create instances with minio object storage/*
+	/** /
+	'objectstore' =>
+	array (
+		'class' => 'OC\\Files\\ObjectStore\\S3',
+		'arguments' =>
+		array (
+			'bucket' => 'nc-' . $hostname,
+			'key' => 'nextcloud',
+			'secret' => 'nextcloud',
+			'hostname' => 'minio',
+			'port' => '9000',
+			'use_ssl' => false,
+			'use_path_style' => true,
+		),
+	),
+
+	/** /
+	'objectstore_multibucket' => array(
+		'class' => 'OC\\Files\\ObjectStore\\S3',
+		'arguments' => array(
+			// optional, defaults to 64
+			'num_buckets' => 64,
+			// n integer in the range from 0 to (num_buckets-1) will be appended
+			'bucket' => 'nextcloud_',
+			'bucket' => 'nc-' . $hostname,
+			'key' => 'nextcloud',
+			'secret' => 'nextcloud',
+			'hostname' => 'minio',
+			'port' => '9000',
+			'use_ssl' => false,
+			'use_path_style' => true,
+		),
+	),/**/
 );
