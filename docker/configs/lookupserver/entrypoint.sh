7fe7cf4d32ca2505bbe7bc75978577b133d730ba
Julius Härtl <jus@bitgrid.net>
2022-01-05 13:22:12 +0100

    Fix shellcheck
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- docker/configs/lookupserver/entrypoint.sh
+++ docker/configs/lookupserver/entrypoint.sh
@@ -4,10 +4,10 @@ sleep 5 && \
 service cron start && \
 rm /var/run/apache2/apache2.pid 2>/dev/null || true && \
 (
-    if [ $(mysql -u root -pabrakadabra -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'nextclousd')" | tail -n1) -eq 0 ]
-    then
+    TABLE_EXISTS=$(mysql -u root -pabrakadabra -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'lookup')" | tail -n1)
+    if [[ "$TABLE_EXISTS" -eq "0" ]]; then
         mysql -u root -pabrakadabra -e "CREATE USER 'lookup' IDENTIFIED BY 'lookup'; GRANT USAGE ON lookup.* TO 'lookup'@'%' IDENTIFIED BY 'lookup'; GRANT ALL privileges ON lookup.* TO 'lookup'@'%'; FLUSH PRIVILEGES;" && \
-        mysql -u root -pabrakadabra < /root/mysql.dmp && \
+        mysql -u root -pabrakadabra < /root/mysql.dmp
     fi
 ) && \
 /usr/sbin/apache2ctl -D FOREGROUND
