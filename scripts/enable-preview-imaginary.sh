b927e2a0d1c4d3c9ac3c1c7d837ccf82e8492d88
Carl Schwan <carl@carlschwan.eu>
2022-05-19 17:09:53 +0200

    Add doc for imginary
    and add a script to automatic set the configs
    
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

---
+++ scripts/enable-preview-imaginary.sh
@@ -0,0 +1,13 @@
+
+function OCC () {
+    docker compose exec nextcloud sudo -E -u www-data "./occ" "$@"
+}
+
+OCC config:system:set enabledPreviewProviders 0 --value 'OC\Preview\MP3'
+OCC config:system:set enabledPreviewProviders 1 --value 'OC\Preview\TXT'
+OCC config:system:set enabledPreviewProviders 2 --value 'OC\Preview\MarkDown'
+OCC config:system:set enabledPreviewProviders 3 --value 'OC\Preview\OpenDocument'
+OCC config:system:set enabledPreviewProviders 4 --value 'OC\Preview\Krita'
+OCC config:system:set enabledPreviewProviders 4 --value 'OC\Preview\Imaginary'
+
+OCC config:system:set preview_imaginary_url --value 'http://previews_hpb:8088'
