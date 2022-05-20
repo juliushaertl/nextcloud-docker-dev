9cfb62c2523fe8d0802d9f04fdddeb3c22aea58c
Julius Härtl <jus@bitgrid.net>
2022-05-20 10:07:24 +0200

    Move object storage enabling to environment variable
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- README.md
+++ README.md
@@ -264,8 +264,7 @@ docker-compose up -d elasticsearch elasticsearch-ui
 
 ## Object storage
 
-In `data/additional.config.php`, uncomment the 'objectstore' config and
-then start docker with minio enabled.
+Primary object storage can be enabled by setting the `PRIMARY=minio` environment variable either in your .env file or in docker-compose.yml for individual containers.
 
 ```bash
 docker-composer up proxy nextcloud minio
--- data/additional.config.php
+++ data/additional.config.php
@@ -1,51 +1,69 @@
 <?php
-
+// FIXME: Move everything except the last part to the containers
 // set the hostname to the VIRTUAL_HOST set for the docker container, otherwise fallback to the docker hostname
 $hostname = gethostname();
+$primary = isset($_ENV['PRIMARY']) ? $_ENV['PRIMARY'] : '';
 $virtualHost = isset($_ENV['VIRTUAL_HOST']) ? $_ENV['VIRTUAL_HOST'] : '';
 $virtualHost = explode(".", $virtualHost);
 if (count($virtualHost) > 0) {
 	$hostname = array_shift($virtualHost);
 }
 
-$CONFIG = array (
-	//  'htaccess.RewriteBase' => '/',
-
-	// Uncomment to create instances with minio object storage
-	/* /
-	'objectstore' =>
-	array (
-		'class' => 'OC\\Files\\ObjectStore\\S3',
-		'arguments' =>
+$CONFIG = [];
+if ($primary === 'minio') {
+	$CONFIG += [
+		'objectstore' =>
 		array (
-			'bucket' => 'nc-' . $hostname,
-			'key' => 'nextcloud',
-			'secret' => 'nextcloud',
-			'hostname' => 'minio',
-			'port' => '9000',
-			'use_ssl' => false,
-			'use_path_style' => true,
-			'autocreate' => true,
-		),
-	),
-	/* */
+			'class' => 'OC\\Files\\ObjectStore\\S3',
+			'arguments' =>
+			array (
+				'bucket' => 'nc-' . $hostname,
+				'key' => 'nextcloud',
+				'secret' => 'nextcloud',
+				'hostname' => 'minio',
+				'port' => '9000',
+				'use_ssl' => false,
+				'use_path_style' => true,
+				'autocreate' => true,
+				'verify_bucket_exists' => true,
+			),
+		)
+	];
+}
 
-	/* /
-	'objectstore_multibucket' => array(
-		'class' => 'OC\\Files\\ObjectStore\\S3',
-		'arguments' => array(
-			// optional, defaults to 64
-			'num_buckets' => 64,
-			// n integer in the range from 0 to (num_buckets-1) will be appended
-			'bucket' => 'nextcloud_',
-			'bucket' => 'nc-' . $hostname,
-			'key' => 'nextcloud',
-			'secret' => 'nextcloud',
-			'hostname' => 'minio',
-			'port' => '9000',
-			'use_ssl' => false,
-			'use_path_style' => true,
+if ($primary === 'minio-multibucket') {
+	$CONFIG += [
+		'objectstore_multibucket' => array(
+			'class' => 'OC\\Files\\ObjectStore\\S3',
+			'arguments' => array(
+				// optional, defaults to 64
+				'num_buckets' => 64,
+				// n integer in the range from 0 to (num_buckets-1) will be appended
+				'bucket' => 'nextcloud_',
+				'bucket' => 'nc-' . $hostname,
+				'key' => 'nextcloud',
+				'secret' => 'nextcloud',
+				'hostname' => 'minio',
+				'port' => '9000',
+				'use_ssl' => false,
+				'use_path_style' => true,
+			),
 		),
-	),
-	/* */
+	];
+}
+
+// Useful config values during development
+$CONFIG += array(
+	'debug' => true,
+	'loglevel' => 2,
+
+	//  'htaccess.RewriteBase' => '/',
+	'log_query' => false,
+	'query_log_file' => '/var/www/html/data/query.log',
+
+	'diagnostics.logging' => false,
+	'diagnostics.logging.threshold' => 0,
+	'log.condition' => [
+        'apps' => ['diagnostics', 'admin_audit'],
+	],
 );
