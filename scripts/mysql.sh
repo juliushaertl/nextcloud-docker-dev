c61c7ab837443455064ca431b2b1e058533e44d9
Christian Wolf <github@christianwolf.email>
2023-03-16 11:04:05 +0100

    Added DB script to access mysql storage
    Signed-off-by: Christian Wolf <github@christianwolf.email>

---
+++ scripts/mysql.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Default container name to operate on
+instance=${INSTANCE:-nextcloud}
+
+show_help() {
+    cat << EOF
+$0: Run commands in the database backend directly.
+Usage:
+    $0 [PARAMS] -- {MySQL Params}
+
+The PARAMS are interpreted by the wrapper script while the MySQL Params are forwarded literally to the database client.
+To separate the two lists, the double dash can be used.
+
+The following parameters are recognized by the wrapper:
+  --instance/-i INST     Use the database for the container INST. (Default: nextcloud)
+  --help/-h              Show this help and exit.
+EOF
+}
+
+run_mysql() {
+    docker compose exec database-mysql mysql -u nextcloud -pnextcloud "$instance" "$@"
+    exit $?
+}
+
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        --instance|-i)
+            instance="$2"
+            # Additional shift
+            shift
+            ;;
+        --help|-h)
+            show_help
+            exit
+            ;;
+        --)
+            shift
+            run_mysql "$@"
+            ;;
+        *)
+            echo "Parameter $1 is not recognized. I assume this is for the mysql client."
+            run_mysql "$@"
+            ;;
+    esac
+
+    # Shift to the next parameter
+    shift
+done
+
+# No parameters were given.
+run_mysql
+exit $?
