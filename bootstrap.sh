ee4cc0aa599a3f27d7197e13a7748b838d428968
Florent <florent.fayolle69@gmail.com>
2023-04-05 08:54:10 +0200

    Update bootstrap.sh : oci not supported yet
    Co-authored-by: Julius Härtl <jus@bitgrid.net>

--- bootstrap.sh
+++ bootstrap.sh
@@ -98,7 +98,8 @@ NEXTCLOUD_AUTOINSTALL_APPS="${NEXTCLOUD_AUTOINSTALL_APPS[@]}"
 DOCKER_SUBNET=192.168.21.0/24
 PORTBASE=821
 XDEBUG_MODE=develop
-SQL=mysql # other values: "pgsql", "oci"
+# SQL variant to use, possible values: sqlite, mysql, pgsql
+SQL=mysql
 DB_SERVICE=database-mysql # other values: "database-postgres"
 EOT
 fi
