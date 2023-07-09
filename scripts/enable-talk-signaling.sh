1c8210f139913960a90b8fed1fcb916bde9849c8
Marcel Müller <marcel-mueller@gmx.de>
2023-07-09 14:58:57 +0200

    Add signaling container for talk
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

---
+++ scripts/enable-talk-signaling.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]
+  then
+    echo "Usage $0 CONTAINER"
+	exit 1
+fi
+
+CONTAINER=$1
+
+function occ() {
+    docker compose exec "$CONTAINER" sudo -E -u www-data "./occ" "$@"
+}
+
+# shellcheck disable=SC1091
+source .env
+
+echo "Setting up talk signaling with http://talk-signaling$DOMAIN_SUFFIX on $CONTAINER"
+docker-compose up -d talk-signaling
+
+if ! occ talk:signaling:list --output="plain" | grep -q "http://talk-signaling$DOMAIN_SUFFIX"; then
+  occ talk:signaling:add "http://talk-signaling$DOMAIN_SUFFIX" "1234"
+fi
+
