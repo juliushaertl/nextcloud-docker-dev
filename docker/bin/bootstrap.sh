855cfa4b2a74fd4e11937938f405bb8e62e310d7
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2024-10-17 10:10:37 +0200

    feat: add basic support for Oracle DB
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

--- docker/bin/bootstrap.sh
+++ docker/bin/bootstrap.sh
@@ -100,7 +100,7 @@ wait_for_other_containers() {
 		"oci")
 			output " - Oracle"
 			retry_with_timeout "(echo > /dev/tcp/database-$SQL/1521) 2>/dev/null" 30 "⚠ Unable to connect to the Oracle server"
-			sleep 2
+			sleep 45
 			;;
 		"sqlite")
 			output " - SQLite"
@@ -233,7 +233,7 @@ install() {
 
 	output "🔧 Starting auto installation"
 	if [ "$SQL" = "oci" ]; then
-		OCC maintenance:install --admin-user=$USER --admin-pass=$PASSWORD --database="$SQL" --database-name=nextcloud --database-host="$SQLHOST" --database-port=1521 --database-user=nextcloud --database-pass=nextcloud
+		OCC maintenance:install --admin-user=$USER --admin-pass=$PASSWORD --database="$SQL" --database-name=FREE --database-host="$SQLHOST" --database-port=1521 --database-user=system --database-pass=oracle
 	elif [ "$SQL" = "pgsql" ]; then
 		OCC maintenance:install --admin-user=$USER --admin-pass=$PASSWORD --database="$SQL" --database-name="$DBNAME" --database-host="$SQLHOST" --database-user=postgres --database-pass=postgres
 	elif [ "$SQL" = "mysql" ]; then
